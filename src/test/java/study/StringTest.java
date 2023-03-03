5ea2ed5247f5562c8c3bfed85db847e11538b46b
soonable@naver.com <soonable@naver.com>
2023-03-03 16:20:45 +0900

    test : 요구사항 3 (charAt, Exception, DisplayName)

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -14,7 +16,7 @@ public class StringTest {
     @Test
     void split() {
         String[] arr = "1,2".split(",");
-        assertThat(arr).containsExactly("1","2");
+        assertThat(arr).containsExactly("1", "2");
     }
 
     @Test
@@ -25,8 +27,24 @@ public class StringTest {
 
     @Test
     void substring() {
-        String actual = "(1,2)".substring(1,4);
+        String actual = "(1,2)".substring(1, 4);
         assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt 메서드 학습테스트")
+    void charAt() {
+        char c = "abc".charAt(0);
+        assertThat(c).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName("charAt IndexOutOfBounds 테스트")
+    void charAtIndexOutOfBounds() {
+        assertThatThrownBy(() -> {
+            "abc".charAt(5);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 5");
     }
 
 }
