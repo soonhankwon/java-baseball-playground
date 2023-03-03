67db3edec484910036af263cc2ce0bb7a9aa7f6f
soonable@naver.com <soonable@naver.com>
2023-03-03 14:43:07 +0900

    test : 요구사항 1,2 (split, substring) 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,23 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() {
+        String[] arr = "1,2".split(",");
+        assertThat(arr).containsExactly("1","2");
+    }
+
+    @Test
+    void splitOneElementCase() {
+        String[] arr = "1".split(",");
+        assertThat(arr).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String actual = "(1,2)".substring(1,4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
 }
