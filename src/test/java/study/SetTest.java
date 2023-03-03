da532892b7702add21b7734ae09d979b292199e4
soonable@naver.com <soonable@naver.com>
2023-03-03 16:46:08 +0900

    test : Set Collection 요구사항 3 학습테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -30,9 +31,9 @@ public class SetTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3})
-    @DisplayName("contains() 메서드 학습테스트")
-    void contains(int number) {
-        assertThat(numbers.contains(number)).isTrue();
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("contains() 메서드 학습 테스트, 입력 값에 따라 결과 값이 다른 경우")
+    void contains(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 }
